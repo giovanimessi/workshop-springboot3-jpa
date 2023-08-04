c25c3f92626496ca1434a73ad52ac639295c4e5d
giovani <giovanimouradev@gmail.com>
2023-08-03 21:29:14 -0300

    projeto criado

---
+++ src/main/java/com/treinamentospring/curso/TrenamentospringApplication.java
@@ -0,0 +1,13 @@
+package com.treinamentospring.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenamentospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrenamentospringApplication.class, args);
+	}
+
+}
